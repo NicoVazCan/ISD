adad034feee427c0aad05fbaf0f79a0111e43baf
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2021-10-28 17:20:38 +0200

    [VARIOS] Corregida interfaz FicTripService.

--- ws-app-model/src/main/java/es/udc/ws/app/model/util/FicTripService.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/util/FicTripService.java
@@ -1,7 +1,8 @@
 package es.udc.ws.app.model.util;
 import es.udc.ws.util.exceptions.*;
+
 public interface FicTripService {
-    Excursion crearExcursion(String ciudad, String desc, int precioXPersona, int maxPlazas) throws InputValidationException;
-    Reserva reservarExcursion(Long excursionId, String email, int numPlazas, String targeta)
-            throws InputValidationException, InstanceNotFoundException;
+    Excursion addExcursion(Excursion excursion) throws InputValidationException;
+    Reserva addReserva(Reserva reserva) throws InputValidationException,
+            InstanceNotFoundException;
 }
