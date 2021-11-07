37f3bc0132768aa7c89fb0e5c8443ea45a5ad1a4
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2021-11-08 00:21:17 +0100

    [VARIOS] Añadidas constantes EXCURSION_MARGEN=72 y RESERVA_MARGEN=24 en ModelConstants.

--- ws-app-model/src/main/java/es/udc/ws/app/model/util/ModelConstants.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/util/ModelConstants.java
@@ -1,7 +1,8 @@
 package es.udc.ws.app.model.util;
 
-public class ModelConstants {
-
-    public static final String APP_DATA_SOURCE = "ws-javaexamples-ds";
-
+public class ModelConstants
+{
+    public static final String APP_DATA_SOURCE = "ws-app-ds";
+    public static final short EXCURSION_MARGEN = 72;
+    public static final short RESERVA_MARGEN = 24;
 }
