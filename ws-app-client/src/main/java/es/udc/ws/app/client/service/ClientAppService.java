3e54ccacf56a6894cd430a9a636e04a356402c4b
NicoVazCan <nicolas.vazquez.cancela@udc.es>
2021-12-26 18:24:54 +0100

    [VARIOS] Correción de algunos fallos de la segunda iteración.

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/ClientAppService.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/ClientAppService.java
@@ -2,13 +2,17 @@ package es.udc.ws.app.client.service;
 
 import es.udc.ws.app.client.service.dto.ClientExcursionDto;
 import es.udc.ws.app.client.service.dto.ClientReservaDto;
+import es.udc.ws.app.client.service.exception.ClientFechaComienzoMuyCercaException;
+import es.udc.ws.app.client.service.exception.ClientNoHayTantasPlazasException;
 import es.udc.ws.util.exceptions.InputValidationException;
 import es.udc.ws.util.exceptions.InstanceNotFoundException;
 
 public interface ClientAppService
 {
-    Long addExcursion(ClientExcursionDto excursion) throws InputValidationException;
+    Long addExcursion(ClientExcursionDto excursion) throws InputValidationException,
+            ClientFechaComienzoMuyCercaException;
 
     Long addReserva(ClientReservaDto reserva)
-            throws InstanceNotFoundException, InputValidationException;
+            throws InstanceNotFoundException, InputValidationException,
+            ClientFechaComienzoMuyCercaException, ClientNoHayTantasPlazasException;
 }
